089f5176c3c2c7ebe6cd36be880bf0e808ecd06f
Justin James <justinmjames@protonmail.com>
2022-04-21 22:13:44 -0700

    commit tree 034c57dda24d97bbfcfa608f8ee0d2fd0cb690f3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solutions in this file
+
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+
+function writeCards(names) {
+    let card = [];
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+        card.push(message);
+         
+    }
+
+    return card;
+    
+};
+
+let number = 10
+
+function countDown(number) { 
+    while (number != -1) {
+        console.log(number--);
+        
+    };
+
+};
+
+countDown;
+debugger;
